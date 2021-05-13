3db371f21d2948405d862d6c84b54b3ca23e6a66
Liquid Luke <83419712+liquidLu@users.noreply.github.com>
2021-05-13 08:14:01 +0530

    Add burner contract

---
+++ contracts/Burner.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+contract Burner {
+    string public name;
+
+    constructor() public {
+        name = "LQDR Buner Contract";
+    }
+}
